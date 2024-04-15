40cd6ae7718ac7c3a2dab43b2d6518b35c0fb200
jackemick <jackemick20@gmail.com>
2024-04-15 02:53:23 -0400

    full site
    only issue i have is the api key not working outside my .env

---
+++ my-app/src/ApiContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from 'react';
+const apiKey = process.env.WEATHER_API_KEY;
+const ApiContext = createContext();
+
+export function ApiProvider({ children }) {
+  const [data, setData] = useState(null);
+
+  const fetchData = async (location) => {
+    try {
+      const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+ location +'?key=' + apiKey + '2&include=days');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <ApiContext.Provider value={{ data, fetchData }}>
+      {children}
+    </ApiContext.Provider>
+  );
+}
+
+export function useApi() {
+  return useContext(ApiContext);
+}
+
